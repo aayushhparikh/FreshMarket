49f5285b9a3ef3a92b87b62a3b73d98388765201
Aayush Parikh <59831646+aayushparikh@users.noreply.github.com>
2022-11-22 15:44:39 -0500

    added arraylist/arrayadapter

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    GridView items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        items = findViewById(R.id.itemsGrid);
+        ArrayList<itemList> itemListArrayList = new ArrayList<itemList>();
+
+        itemListArrayList.add(new itemList("Apples"));
+        itemListArrayList.add(new itemList("Oranges"));
+        itemListArrayList.add(new itemList("Bananas"));
+        itemListArrayList.add(new itemList("Pear"));
+        itemListArrayList.add(new itemList("Watermelon"));
+        itemListArrayList.add(new itemList("Avacado"));
+
+        ItemAdapter adapter = new ItemAdapter(this, itemListArrayList);
+        items.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/example/finalproject/itemList.java
@@ -0,0 +1,17 @@
+package com.example.finalproject;
+
+public class itemList {
+    private String itemname;
+
+    public itemList(String itemname){
+        this.itemname = itemname;
+    }
+
+    public String getItemname() {
+        return itemname;
+    }
+
+    public void setItemname(String itemname){
+        this.itemname = itemname;
+    }
+}
