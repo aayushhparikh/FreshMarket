767723a3989b7eeb36b197c7d152fd936c0ac894
Aayush Parikh <59831646+aayushparikh@users.noreply.github.com>
2022-11-26 20:04:42 -0500

    adding addToCart functionality

--- app/src/main/java/com/example/finalproject/Adapter.java
+++ app/src/main/java/com/example/finalproject/Adapter.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -14,17 +15,21 @@ import androidx.recyclerview.widget.RecyclerView;
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
 
     private Context context;
     private Activity activity;
     private ArrayList grocery_name;
+    private Listener listener;
 
-    Adapter(Activity activity, Context context, ArrayList grocery_name) {
+
+    Adapter(Activity activity, Context context, ArrayList grocery_name, Listener listener) {
         this.activity = activity;
         this.context = context;
         this.grocery_name = grocery_name;
+        this.listener = listener;
     }
 
     @NonNull
@@ -32,7 +37,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(context);
         View view = inflater.inflate(R.layout.card_item, parent, false);
-        return new MyViewHolder(view);
+        return new MyViewHolder(view, listener);
     }
 
     @Override
@@ -46,14 +51,29 @@ public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
         return grocery_name.size();
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView name_id;
         LinearLayout mainLayout;
+        Button addToCart;
+        Listener listener;
 
-        public MyViewHolder(@NonNull View itemView) {
+        public MyViewHolder(@NonNull View itemView, Listener listener) {
             super(itemView);
             name_id = itemView.findViewById(R.id.name_id);
             mainLayout = itemView.findViewById(R.id.mainLayout);
+            addToCart = itemView.findViewById(R.id.addtocart);
+            this.listener = listener;
+
+            addToCart.setOnClickListener(this);
         }
+
+        @Override
+        public void onClick(View view) {
+            listener.addToCartClicked(view, getAdapterPosition());
+        }
+    }
+
+    public interface Listener{
+        void addToCartClicked(View view, int position);
     }
 }
--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.finalproject;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -7,20 +9,22 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Currency;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Adapter.Listener {
     RecyclerView recyclerView;
 
-    Button cart, signin;
+    Button cart, signin, addtocart;
 
     ArrayList<String> grocery_name;
 
@@ -33,18 +37,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recyclerView = findViewById(R.id.recyclerview);
 
+        //defining variables
+        recyclerView = findViewById(R.id.recyclerview);
 
         cart = findViewById(R.id.cart);
         signin = findViewById(R.id.signin2);
+        addtocart = findViewById(R.id.addtocart);
 
         myDb = new DBHelper(this, null, null, 1);
         grocery_name = new ArrayList<>();
 
+        //calling store array data to store db values into array
         StoreArrayData();
 
-        adapter = new Adapter(MainActivity.this, this, grocery_name);
+        adapter = new Adapter(MainActivity.this, this, grocery_name, this);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
 
@@ -63,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
 
     void StoreArrayData() {
@@ -72,12 +80,16 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void addData(){
+    public void addData() {
         itemList item1 = new itemList("apple", 10);
-        itemList item2= new itemList("oranges", 4);
+        itemList item2 = new itemList("oranges", 4);
 
         myDb.insertData(item1);
         myDb.insertData(item2);
     }
 
+    @Override
+    public void addToCartClicked(View view, int position) {
+        Toast.makeText(MainActivity.this, "Clicked", Toast.LENGTH_SHORT).show();
+    }
 }
